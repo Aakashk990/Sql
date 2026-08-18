0e3a97f6ab5d6c1d105251a9883fb1055d0e2287
Aakashk990 <aakashk990@gmail.com>
2026-08-19 01:39:51 +0530

    Create select_queries.sql
    Day 1

---
+++ select_queries.sql
@@ -0,0 +1,10 @@
+--Question 1 List the third and fourth largest cities (by population) in the United States and their population 
+SELECT city,Population 
+FROM north_american_cities 
+WHERE Country='United States'
+ORDER BY Population desc limit 2 offset 2;
+
+--o/p
+City	Population
+Chicago	2718782
+Houston	2195914
