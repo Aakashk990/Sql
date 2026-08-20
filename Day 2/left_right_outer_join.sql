b6df034d0db005fa7ddad14b1c5e47b8dd3ae363
Aakashk990 <aakashk990@gmail.com>
2026-08-21 00:53:34 +0530

    q5

--- Day 2/left_right_outer_join.sql
+++ Day 2/left_right_outer_join.sql
@@ -6,3 +6,17 @@ select distinct(b.Building_name) from Buildings b left join Employees e on b.Bui
 -- 1e
 -- 2w
 
+
+--question 2 List all buildings and the distinct employee roles in each building (including empty buildings) 
+select distinct(Building_name), Role from Buildings 
+left join
+Employees on Buildings.Building_name = Employees.Building;
+
+-- o/p
+-- Building_name	Role
+-- 1e	Engineer
+-- 1e	Manager
+-- 1w	
+-- 2e	
+-- 2w	Artist
+-- 2w	Manager
