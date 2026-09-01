bf8174f3aff19216449ab095569ed9b1944b0b35
Aakashk990 <aakashk990@gmail.com>
2026-09-02 01:53:35 +0530

    q5

--- Day 5/groupby2.sql
+++ Day 5/groupby2.sql
@@ -1,2 +1,5 @@
 -- Find the number of Artists in the studio (without a HAVING clause)
 select count(role) from employees where role = 'Artist';
+
+--Find the number of Employees of each role in the studio 
+select Role,count(Name) from employees group by Role;
