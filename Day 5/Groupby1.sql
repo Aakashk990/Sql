aca85528f62d4510accfdfda1b19e34adca5b7f8
Aakashk990 <aakashk990@gmail.com>
2026-09-02 01:47:49 +0530

    q2

--- Day 5/Groupby1.sql
+++ Day 5/Groupby1.sql
@@ -1,2 +1,6 @@
 --Find the longest time that an employee has been at the studio 
 select max(Years_employed) from Employees;
+
+
+--For each role, find the average number of years employed by employees in that role
+select Role,avg(Years_employed) from Employees group by role;
