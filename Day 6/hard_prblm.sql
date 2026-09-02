ef5d32c3e5bf305863951b145723bd480c07bfa1
Aakashk990 <aakashk990@gmail.com>
2026-09-02 23:49:20 +0530

    hard_prblm

---
+++ Day 6/hard_prblm.sql
@@ -0,0 +1,20 @@
+Problem
+A group of passengers is waiting in line to board a bus. However, the bus has a maximum weight capacity of 1000 kilograms. 
+Passengers board one at a time based on their boarding order, and the boarding process stops as soon as 
+the next passenger would cause the total weight to exceed the limit.
+
+Input:
+
+mcb_sample:
+
+passenger_id	passenger_name	weight_kg	boarding_order
+5	            Alice	        250	        1
+4	            Bob	            175	        5
+3	            Alex	        350	        2
+6	            John Cena	    400	        3
+
+
+Output:
+
+passenger_name
+John Cena
