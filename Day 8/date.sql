ad0064abfa519f156b6825860ce3927a4be60eae
Aakashk990 <aakashk990@gmail.com>
2026-09-06 23:58:16 +0530

    DATE_TRUNC query

--- Day 8/date.sql
+++ Day 8/date.sql
@@ -33,3 +33,5 @@ SELECT DATE_TRUNC('week', login_timestamp) AS login_week,
        COUNT(DISTINCT user_id) AS wau
 FROM logins
 GROUP BY 1 ORDER BY 1;
+
+DATE_TRUNC('month', '2024-03-15') returns 2024-03-01; 'week' returns the Monday of that ISO week on most platforms (BigQuery and MySQL default weeks to Sunday, verify on yours).
