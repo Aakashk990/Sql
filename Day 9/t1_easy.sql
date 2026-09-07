e5b5a68f20f4e93e84c1c056c0fc4c9a0b502cbe
Aakashk990 <aakashk990@gmail.com>
2026-09-08 01:56:34 +0530

    o/p t1 easy

--- Day 9/t1_easy.sql
+++ Day 9/t1_easy.sql
@@ -4,3 +4,16 @@
 
 
 select order_id,customer_name,order_date from orders where order_date between '2023-06-01' and '2023-12-31' order by order_date;
+
+
+o/p
+
+"order_id"	"customer_name"	"order_date"
+7	"Sneha"	"2023-06-25"
+8	"Ananya"	"2023-07-09"
+9	"Vikram"	"2023-08-14"
+10	"Priya"	"2023-09-02"
+11	"Meera"	"2023-10-11"
+12	"Karan"	"2023-11-20"
+13	"Arjun"	"2023-12-05"
+14	"Ananya"	"2023-12-25"
