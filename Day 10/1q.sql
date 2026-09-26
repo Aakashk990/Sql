21dc95ebe4d16e5d3148d4eec2b6a966cef43728
Aakashk990 <aakashk990@gmail.com>
2026-09-26 20:18:07 +0530

    ch

--- Day 10/1q.sql
+++ Day 10/1q.sql
@@ -6,6 +6,13 @@ Important: Syntax mapping (read this first)
 --in Postgres docs you'll find nothing, and that's expected, not a mistake on your end. Here's what Postgres uses instead:
 
 
+
+
+
+
+
+
+
 SQL Server / MySQL	PostgreSQL equivalent
 DATEADD(day, 7, order_date)	order_date + INTERVAL '7 days'
 DATEDIFF(day, date1, date2)	date2 - date1 (for DATE columns), or AGE(date2, date1) / EXTRACT(EPOCH FROM (ts2 - ts1)) for timestamps
